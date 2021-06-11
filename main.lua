2bf34b414c9377855941532cd1749502e283c640
unknown <misha@burnayev.com>
2021-06-11 19:18:00 -0400

    Updated Ver

--- main.lua
+++ main.lua
@@ -2,31 +2,32 @@ local bonusItems = RegisterMod("Bonus Items!", 1)
 
 function bonusItems:givePlayeritem(player)
     local level = Game():GetLevel():GetStage()
-    local roomType = Game():GetRoom():GetType()
+    -- local roomType = Game():GetRoom():GetType()
     player = Isaac.GetPlayer(0);
-
-    -- if roomType == RoomType.ROOM_DEFAULT then
-    --     roomPool = ItemPoolType.POOL_GOLDEN_CHEST
-    -- else
-    --     roomPool = Game():GetItemPool():GetPoolForRoom(roomType, seed)
-    -- end
-    -- if roomPool == ItemPoolType.POOL_NULL then
-    --     roomPool = ItemPoolType.POOL_TREASURE 
-    -- end
-    roomPool = ItemPoolType.POOL_TREASURE
+    roomPool = ItemPoolType.POOL_ANGEL
+    -- TODO: randomize the item pools that you can get an item from
     findCollectible = Game():GetItemPool():GetCollectible(roomPool, false, seed, CollectibleType.COLLECTIBLE_NULL)
     
     -- for stage in level do
     --     player:AddCollectible(findCollectible)
     --     print("item given!")
     -- end
-    cap = math.random(1, 5)
-    i = 0
-    while i < cap do 
+    cap = math.random(1, 3)
+    -- print(cap)
+    counter = 0
+    for num = 1,cap do
         player:AddCollectible(findCollectible)
-        i = i + 1
+        counter = counter + 1
+    end
+    -- TODO: make it so that you can't get active items, just to avoid potentially losing an already godd active item
+
+    if counter == 1 then
+        print(counter .. " item was given!")
+    else
+        print(counter .. " items were given!")
     end
 end
--- TODO: fix this so the game doesn't crash
+-- TODO: find a way to get a variety of items instead of duplicates of one
 
-bonusItems:AddCallback(ModCallbacks.MC_POST_PLAYER_RENDER, bonusItems.givePlayeritem)
+bonusItems:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, bonusItems.givePlayeritem)
+-- TODO: find a way to receive item(s) each floor rather than the first time you generate your character
