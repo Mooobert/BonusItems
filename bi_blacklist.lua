863362a585c244d6dd738eddc001ca03d1facd94
unknown <misha@burnayev.com>
2021-06-14 10:26:23 -0400

    blacklist integration

---
+++ bi_blacklist.lua
@@ -0,0 +1,61 @@
+--[=[ 
+ 
+to add items to the item blacklist add them to the list separated by a space.
+e.g:
+
+local itemsBlacklistString = [[
+258
+721
+]]
+
+would result in the items with the ids 258 (Missing No) and 721 (TMTRAINER) being blacklisted from the other Tainted Lazarus.
+
+Note that the mod generates items based on the previous item pool used, so it shouldn't give items that aren't in the pool/have been removed from the pool/have been taken already.
+--]=]
+
+local itemsBlacklistString = [[
+]]
+
+local bi_items_blacklist = {} -- module
+
+local generateBlacklist = {}
+
+local illegalItems = {
+    -- story related items
+	[238]=true, -- Key Piece 1
+	[239]=true, -- Key Piece 2
+	[327]=true, -- The Negative
+	[328]=true, -- The Polaroid
+	[550]=true, -- Broken Shovel 1
+	[551]=true, -- Broken Shovel 2
+	[552]=true, -- Mom's Shovel
+	[626]=true, -- Knife Piece 1
+	[627]=true, -- Knife Piece 2
+	[633]=true, -- Dogma
+	[668]=true  -- Dad's Note
+    -- undesirable/run ruining items
+    []=true, -- Bob's brain
+    []=true, -- Wiz
+    -- potentially major run altering items
+    []=true, -- Dead Cat
+    []=true, -- TMTRAINER
+    []=true, -- Ipecac
+    []=true, -- Soy Milk
+    []=true, -- Missing No
+    []=true, -- Polyphemus
+    []=true, -- Libra
+}
+
+for i in string.gmatch(itemsBlacklistString, "%S+") do
+	generateBlacklist[tonumber(i)] = true
+end
+
+function bi_items_blacklist.canRollInto(ID)
+	return generateBlacklist[ID] == nil
+end
+
+function bi_items_blacklist.isIllegalItem(ID)
+	return illegalItems[ID] ~= nil
+end 
+
+return bi_items_blacklist
