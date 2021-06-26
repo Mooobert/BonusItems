ab48f57aee1c37c69b7155d31f34bc26e3719142
unknown <misha@burnayev.com>
2021-06-25 23:56:39 -0400

    crash fix

--- main.lua
+++ main.lua
@@ -1,53 +1,73 @@
 local bonusItems = RegisterMod("Bonus Items!", 1)
 
--- local bi_blaklist = include("bi_blacklist")
--- local bonusItems_blacklist
+local bi_blacklist = include("bi_blacklist")
+
+local pickupIncoming = {}
+local playerCounter = 1
 
 function bonusItems:choosePool(player)
     local item_pools = {
-        ItemPoolType.POOL_TREASURE,
-        ItemPoolType.POOL_DEVIL,
-        ItemPoolType.POOL_TREASURE,
-        ItemPoolType.POOL_ANGEL,
-        ItemPoolType.POOL_TREASURE,
-        ItemPoolType.POOL_SECRET,
-        ItemPoolType.POOL_TREASURE,
-        ItemPoolType.POOL_GOLDEN_CHEST,
-        ItemPoolType.POOL_TREASURE
+        ItemPoolType.POOL_TREASURE, -- 0
+        ItemPoolType.POOL_DEVIL, -- 3
+        ItemPoolType.POOL_TREASURE, -- 0
+        ItemPoolType.POOL_ANGEL, -- 4
+        ItemPoolType.POOL_TREASURE, -- 0
+        ItemPoolType.POOL_SECRET, -- 5
+        ItemPoolType.POOL_TREASURE, -- 0
+        ItemPoolType.POOL_GOLDEN_CHEST, -- 8
+        ItemPoolType.POOL_TREASURE -- 0
     }
     roomPool = item_pools[math.random(1,9)]
     print(roomPool)
 end
 
 function bonusItems:giveNewItem(player)
+    local queuedItem = player.QueuedItem.Item
+    if queuedItem ~= nil and playerData["pickupIncoming"] ~= true then
+        local qID = queuedItem.ID
+        if not bi_items_blacklist.isIllegalItem(qID) then
+            playerData["pickupIncoming"] = true
+        end
+    end
+
+    -- if playerData["pickupIncoming"] == true then
+    --     if queuedItem == nil then
+    --         local level = Game():GetLevel():GetStage()
+    --         local roomType = Game():GetRoom():GetType()
+    --         local itemConfig = Isaac.GetItemConfig()
+    --         bonusItems:choosePool(player)
+    --     end
+    -- end
+
+    bonusItems:choosePool(player)
     findCollectible = Game():GetItemPool():GetCollectible(roomPool, false, seed, CollectibleType.COLLECTIBLE_NULL)
     player:AddCollectible(findCollectible)
 end
 
 -- function bonusItems:removeItem(player)
 --     if findCollectible == ItemType.ITEM_ACTIVE then
+--     if findCollectible == CollectibleType.COLLECTIBLE_REVELATION then
 --         player:RemoveCollectible(findCollectible)
 --         print(findCollectible .. " was removed!")
 --     end
 -- end
 
 function bonusItems:itemsPlease(player)
-    local level = Game():GetLevel():GetStage()
-    -- local roomType = Game():GetRoom():GetType()
-    player = Isaac.GetPlayer(0);
+    player = Isaac.GetPlayer(1);
+    playerData = player:GetData()
+    if playerCounter == 1 then
+        playerData["pickupIncoming"] = true
+    end
 
     cap = math.random(1, 3)
-    counter = 0
     for num = 1,cap do
-        bonusItems:choosePool(player)
         bonusItems:giveNewItem(player)
         print(findCollectible .. " was given")
     end
     print("-----")
-    -- TODO: make it so that you can't get active items since currently held active items trump ones given by the mod
+    -- TODO: make it so that you can't get active items, just to avoid potentially losing an already good active item
 end
 
 bonusItems:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, bonusItems.itemsPlease)
-
 -- TODO: find a way to receive item(s) each floor rather than the first time you generate your character
--- TODO: add game-crashing items to blacklist 
+
