e1c6b41039831f8e2bf473927c7e5734746a03e4
unknown <misha@burnayev.com>
2021-07-01 21:55:58 -0400

    items added

--- bi_blacklist.lua
+++ bi_blacklist.lua
@@ -8,12 +8,30 @@ local itemsBlacklistString = [[
 721
 ]]
 
-would result in the items with the ids 258 (Missing No) and 721 (TMTRAINER) being blacklisted from the other Tainted Lazarus.
+would result in the items with the ids 258 (Missing No) and 721 (TMTRAINER) being blacklisted.
 
 Note that the mod generates items based on the previous item pool used, so it shouldn't give items that aren't in the pool/have been removed from the pool/have been taken already.
 --]=]
 
 local itemsBlacklistString = [[
+    273
+    358
+    209
+    371
+    5
+    81
+    721
+    149
+    330
+    258
+    169
+    304
+    402
+    561
+    689
+    316
+    315
+    276
 ]]
 
 local bi_items_blacklist = {} -- module
@@ -26,6 +44,10 @@ local illegalItems = {
     [358]=true, -- Wiz
     [209]=true, -- Butt Bombs
     [371]=true, -- Curse of the Tower
+    [5]=true, -- My Reflection
+    [316]=true, -- Cursed Eye
+    [315]=true, -- Strange Attractor
+    [276]=true, -- Isaac's Heart
     -- potentially major run altering items
     [81]=true, -- Dead Cat
     [721]=true, -- TMTRAINER
@@ -35,6 +57,8 @@ local illegalItems = {
     [169]=true, -- Polyphemus
     [304]=true, -- Libra
     [402]=true, -- Chaos
+    [561]=true, -- Almond Milk
+    [689]=true -- Glitched Crown
 }
 
 for i in string.gmatch(itemsBlacklistString, "%S+") do
