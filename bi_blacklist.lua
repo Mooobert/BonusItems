88055dcc81c309d2e28e46da5b4e88264f1ef15e
unknown <misha@burnayev.com>
2021-07-03 11:20:50 -0400

    blacklist cleanup/rework

--- bi_blacklist.lua
+++ bi_blacklist.lua
@@ -9,68 +9,46 @@ local itemsBlacklistString = [[
 ]]
 
 would result in the items with the ids 258 (Missing No) and 721 (TMTRAINER) being blacklisted.
-
-Note that the mod generates items based on the previous item pool used, so it shouldn't give items that aren't in the pool/have been removed from the pool/have been taken already.
 --]=]
 
 local itemsBlacklistString = [[
-    273
-    358
-    209
-    371
-    5
-    81
-    721
-    149
-    330
-    258
-    169
-    304
-    402
-    561
-    689
-    316
-    315
-    276
+273 
+358 
+209 
+371 
+5 
+81 
+721 
+149 
+330 
+258 
+169 
+304 
+402 
+561 
+689 
+316 
+315 
+276 
+222
 ]]
 
 local bi_items_blacklist = {} -- module
 
 local generateBlacklist = {}
 
-local illegalItems = {
-    -- undesirable/run ruining items
-    [273]=true, -- Bob's brain
-    [358]=true, -- Wiz
-    [209]=true, -- Butt Bombs
-    [371]=true, -- Curse of the Tower
-    [5]=true, -- My Reflection
-    [316]=true, -- Cursed Eye
-    [315]=true, -- Strange Attractor
-    [276]=true, -- Isaac's Heart
-    -- potentially major run altering items
-    [81]=true, -- Dead Cat
-    [721]=true, -- TMTRAINER
-    [149]=true, -- Ipecac
-    [330]=true, -- Soy Milk
-    [258]=true, -- Missing No
-    [169]=true, -- Polyphemus
-    [304]=true, -- Libra
-    [402]=true, -- Chaos
-    [561]=true, -- Almond Milk
-    [689]=true -- Glitched Crown
-}
-
 for i in string.gmatch(itemsBlacklistString, "%S+") do
 	generateBlacklist[tonumber(i)] = true
 end
 
 function bi_items_blacklist.canRollInto(ID)
-	return generateBlacklist[ID] == nil
+-- 	-- return generateBlacklist[ID] == nil
+    return generateBlacklist[ID]
 end
 
-function bi_items_blacklist.isIllegalItem(ID)
-	return illegalItems[ID] ~= nil
-end 
+for index, id in pairs(generateBlacklist) do
+    print(index, id)
+end
 
 return bi_items_blacklist
+-- return generateBlacklist
