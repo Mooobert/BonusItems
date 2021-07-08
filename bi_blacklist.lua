500c3e7dbaa0c2b4b59548ed6aaa14f6288f2de1
unknown <misha@burnayev.com>
2021-07-07 21:54:12 -0400

    blacklist functionality restored

--- bi_blacklist.lua
+++ bi_blacklist.lua
@@ -4,36 +4,41 @@ to add items to the item blacklist add them to the list separated by a space.
 e.g:
 
 local itemsBlacklistString = [[
-258
-721
+3
 ]]
 
-would result in the items with the ids 258 (Missing No) and 721 (TMTRAINER) being blacklisted.
+would result in the items with the id 3 (Spoon Bender) being blacklisted from item rolls.
+
+Credit goes to Mr. Worldwide and respectable name chosen by Chips of the Tainted Lazaruz Buff v2 mod 
+https://steamcommunity.com/sharedfiles/filedetails/?id=2491666917&searchtext=tainted+laz+v2
+for serving as inspiration for this mod!
 --]=]
 
 local itemsBlacklistString = [[
-273 
-358 
-209 
-371 
-5 
-81 
-721 
-149 
-330 
-258 
-169 
-304 
-402 
-561 
-689 
-316 
-315 
-276 
-222
+    5
+    81
+    149
+    169
+    206
+    209
+    222
+    258
+    273
+    276
+    304
+    315
+    316
+    330
+    358
+    371
+    378
+    402
+    561
+    689
+    721
 ]]
 
-local bi_items_blacklist = {} -- module
+local bi_blacklist = {} -- module
 
 local generateBlacklist = {}
 
@@ -41,14 +46,12 @@ for i in string.gmatch(itemsBlacklistString, "%S+") do
 	generateBlacklist[tonumber(i)] = true
 end
 
-function bi_items_blacklist.canRollInto(ID)
--- 	-- return generateBlacklist[ID] == nil
+function bi_blacklist.canRollInto(ID)
     return generateBlacklist[ID]
 end
 
-for index, id in pairs(generateBlacklist) do
-    print(index, id)
-end
+-- for index, id in pairs(generateBlacklist) do
+--     print(index)
+-- end
 
-return bi_items_blacklist
--- return generateBlacklist
+return bi_blacklist
