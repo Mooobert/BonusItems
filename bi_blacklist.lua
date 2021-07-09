7a3143d7ec43d8dc73a42283d04c669c7c448184
unknown <misha@burnayev.com>
2021-07-09 19:06:55 -0400

    version cleanup

--- bi_blacklist.lua
+++ bi_blacklist.lua
@@ -1,17 +1,41 @@
 --[=[ 
  
-to add items to the item blacklist add them to the list separated by a space.
+To add items to the item blacklist add them to the list separated by a space.
 e.g:
 
 local itemsBlacklistString = [[
 3
 ]]
 
-would result in the items with the id 3 (Spoon Bender) being blacklisted from item rolls.
+Would result in the items with the id 3 (Spoon Bender) being blacklisted from item rolls.
 
 Credit goes to Mr. Worldwide and respectable name chosen by Chips of the Tainted Lazaruz Buff v2 mod 
 https://steamcommunity.com/sharedfiles/filedetails/?id=2491666917&searchtext=tainted+laz+v2
-for serving as inspiration for this mod!
+for serving as inspiration for this mod and the concept of including a blacklist!
+
+list comes loaded with 20 relatively game changing items:
+
+my reflection
+ipecac
+anti-gravity
+missing no.
+bob's brain
+isaac's heart
+libra
+strange attractor
+cursed eye
+the ludovico technique
+soy milk
+the wiz
+curse of the tower
+no. 2
+chaos
+almond milk
+blood oath
+eye of the occult
+glitched crown
+TMTRAINER
+
 --]=]
 
 local itemsBlacklistString = [[
@@ -36,29 +60,6 @@ local itemsBlacklistString = [[
     689
     721
 ]]
-
--- list comes loaded with 20 relatively game changing items:
---
--- my reflection
--- ipecac
--- anti-gravity
--- missing no.
--- bob's brain
--- isaac's heart
--- libra
--- strange attractor
--- cursed eye
--- the ludovico technique
--- soy milk
--- the wiz
--- curse of the tower
--- no. 2
--- chaos
--- almond milk
--- blood oath
--- eye of the occult
--- glitched crown
--- TMTRAINER
 
 local bi_blacklist = {} -- module
 
