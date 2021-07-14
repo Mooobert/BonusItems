f9b3807894f6c986eda1c9cfaea718d5322038e7
unknown <misha@burnayev.com>
2021-07-13 22:36:30 -0400

    j&e compatibility rework

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local bonusItems = RegisterMod("Bonus Items!", 1)
 
 local bi_blacklist = include("bi_blacklist")
 
+----------------------------------------------------------------------
 function bonusItems:choosePool(player)
     local itemPools = {
         ItemPoolType.POOL_TREASURE, -- 0
@@ -15,38 +16,64 @@ function bonusItems:choosePool(player)
     }
     roomPool = itemPools[math.random(1,8)]
     -- simplest random selection method I could think of that would yield somewhat balanced item pool draws
+    -- print(roomPool)
 end
 
 function bonusItems:giveNewItem(player)
     bonusItems:choosePool(player)
     findCollectible = Game():GetItemPool():GetCollectible(roomPool, false, seed, CollectibleType.COLLECTIBLE_NULL)
+    -- print(findCollectible)
     local itemConfig = Isaac.GetItemConfig()
     collectibleType = itemConfig:GetCollectible(findCollectible).Type
+    -- print(collectibleType)
     -- items fall into 3 categories: collectibleType 1 is passive, 3 is active, and 4 is familiar
 
     if collectibleType == 3 then -- if the chosen item is active, we reroll until we get a decent item
-        -- print("active item " .. findCollectible .. " was found, rerolling...")
+        print("active item " .. findCollectible .. " was found, rerolling...")
         bonusItems:giveNewItem(player)
     elseif bi_blacklist.canRollInto(findCollectible) == true then -- if the chosen item is blacklisted, we also reroll until we get a decent item
-        -- print("blacklisted item " .. findCollectible .. " was found, rerolling...")
+        print("blacklisted item " .. findCollectible .. " was found, rerolling...")
         bonusItems:giveNewItem(player)
     else    
-        -- print("passive item/familiar found!")
+        print("passive item/familiar found!")
         player:AddCollectible(findCollectible)
-        -- print(findCollectible .. " was given")
+        print(findCollectible .. " was given")
+        print("-----")
     end
 end
-
+----------------------------------------------------------------------
 function bonusItems:itemsPlease(player)
-    player = Isaac.GetPlayer(0)
+    -- player = Isaac.GetPlayer(0)
+    -- for i = 1, Game():GetNumPlayers() do
+	-- 	player = Isaac.GetPlayer(i-1)
     cap = math.random(1, 3)
     for num = 1,cap do
         bonusItems:giveNewItem(player)
     end
-    -- print("-----")
+    print('+++++')
 end
+-- end
+    -- player = Isaac.GetPlayer(1)
+    -- cap = math.random(1, 3)
+    -- for num = 1,cap do
+    --     bonusItems:giveNewItem(player)
+    -- end
+    -- print('+++++')
+-- end
+----------------------------------------------------------------------
+function bonusItems:headCount(player)
+    bing = Game():GetNumPlayers()
+    print(bing)
+    for i = 1, Game():GetNumPlayers() do
+        player = Isaac.GetPlayer(i-1)
+        bonusItems:itemsPlease(player)
+    end
+end
+----------------------------------------------------------------------
 
-bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, bonusItems.itemsPlease, EntityType.ENTITY_PLAYER)
+bonusItems:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, bonusItems.headCount)
+-- bonusItems:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, bonusItems.itemsPlease)
+-- bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, bonusItems.itemsPlease, EntityType.ENTITY_PLAYER)
 
 --[[
     pro tip: don't generate items that generate pickups before the level loads or else the game will crash
