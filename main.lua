3ed3a12074456861099a1b4361d9d9ea07913bc4
unknown <misha@burnayev.com>
2021-07-16 22:13:25 -0400

    attempt at jacob and esau compatibility

--- main.lua
+++ main.lua
@@ -2,6 +2,10 @@ local bonusItems = RegisterMod("Bonus Items!", 1)
 
 local bi_blacklist = include("bi_blacklist")
 
+local jacob_type = 19
+local esau_type = 20
+
+
 ----------------------------------------------------------------------
 function bonusItems:choosePool(player)
     local itemPools = {
@@ -45,9 +49,6 @@ function bonusItems:giveNewItem(player)
 end
 ----------------------------------------------------------------------
 function bonusItems:itemsPlease(player)
-    player = Isaac.GetPlayer(0)
-    -- for i = 1, Game():GetNumPlayers() do
-	-- 	player = Isaac.GetPlayer(i-1)
     cap = math.random(1, 3)
     for num = 1,cap do
         bonusItems:giveNewItem(player)
@@ -64,18 +65,26 @@ end
 -- end
 ----------------------------------------------------------------------
 function bonusItems:headCount(player)
+    player = Isaac.GetPlayer(0)
+    local playerType = player:GetPlayerType()
+    print(playerType)
     bing = Game():GetNumPlayers()
     print(bing)
-    for i = 1, Game():GetNumPlayers() do
-        player = Isaac.GetPlayer(i-1)
+    if playerType == jacob_type or playerType == esau_type then
+        print("nope!")
+    else
+        -- for i = 1, Game():GetNumPlayers() do
+        --     player = Isaac.GetPlayer(i-1)
         bonusItems:itemsPlease(player)
+        -- end
     end
 end
 ----------------------------------------------------------------------
 
 -- bonusItems:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, bonusItems.headCount)
--- bonusItems:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, bonusItems.itemsPlease)
-bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, bonusItems.itemsPlease, EntityType.ENTITY_PLAYER)
+bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, bonusItems.headCount)
+-- bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, bonusItems.itemsPlease)
+-- bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, bonusItems.itemsPlease, EntityType.ENTITY_PLAYER)
 
 --[[
     pro tip: don't generate items that generate pickups before the level loads or else the game will crash
