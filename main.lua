c01a9070c0139af2f4c9d3a2c39777be9e94c395
unknown <misha@burnayev.com>
2021-07-28 01:30:18 -0400

    tweaks and comments

--- main.lua
+++ main.lua
@@ -10,7 +10,9 @@ local destroyed = false
 local itemsDropped = false
 
 ----------------------------------------------------------------------
-function choosePool(player)
+-- Item generation handlers
+----------------------------------------------------------------------
+function chooseItemPool(player)
     local itemPools = {
         ItemPoolType.POOL_TREASURE, -- 0
         ItemPoolType.POOL_DEVIL, -- 3
@@ -28,22 +30,22 @@ end
 function bonusItems:giveNewItem(player)
     local level = Game():GetLevel()
     local pos = Isaac.GetFreeNearPosition(toyboxEntity.Position, 70)
-    choosePool(player)
+    chooseItemPool(player)
     findCollectible = Game():GetItemPool():GetCollectible(roomPool, false, seed, CollectibleType.COLLECTIBLE_NULL)
     local itemConfig = Isaac.GetItemConfig()
     collectibleType = itemConfig:GetCollectible(findCollectible).Type
-    -- items fall into 3 categories: collectibleType 1 is passive, 3s is active, and 4 is familiar (2 is trinkets)
+    -- items fall into 3 categories: collectibleType 1 is passive, 3 is active, and 4 is familiar (2 is trinkets)
 
     if collectibleType == 3 then -- if the chosen item is active, we reroll until we get a decent item
         bonusItems:giveNewItem(player)
     elseif bi_blacklist.canRollInto(findCollectible) == true then -- if the chosen item is blacklisted, we also reroll until we get a decent item
-        bonusItems:giveNewItem(player)
+        bonusItems:giveNewItem(player)   
     else    
         Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, findCollectible, pos, Vector(0, 0), player);
     end
 end
 ----------------------------------------------------------------------
-function bonusItems:itemsPlease(player)
+function itemsPlease(player)
     if itemsDropped == false then
         for i = 1, Game():GetNumPlayers() do
             player = Isaac.GetPlayer(i-1)
@@ -66,58 +68,7 @@ function bonusItems:itemsPlease(player)
     end
 end
 ----------------------------------------------------------------------
-function bonusItems:initToybox()
-    local room = Game():GetRoom()
-
-	if (Game():IsGreedMode() == false and Game():GetLevel():GetCurrentRoomIndex() == Game():GetLevel():GetStartingRoomIndex())
-    or (Game():IsGreedMode() == false and Game():GetLevel():GetCurrentRoomIndex() == 97 and Game():GetLevel():GetStage() == 9)
-	or (Game():IsGreedMode() == true  and Game():GetLevel():GetCurrentRoomIndex() == 98) then
-        local ent = Isaac.FindByType(toyBox, toyboxVar)
-        if #ent > 0 then
-			toyboxEntity = ent[1]
-			if destroyed == true then
-				s = toyboxEntity:GetSprite()
-				s:Play("Destroyed",true)
-			end
-			return
-		end
-
-        if (toyboxEntity == nil) or (toyboxEntity:Exists() == false) then
-			if Game():IsGreedMode() == true then
-			    toyboxEntity = Isaac.Spawn(toyBox, toyboxVar, 0, room:GetGridPosition(112), Vector(0,0), Isaac.GetPlayer(0));
-            else
-                toyboxEntity = Isaac.Spawn(toyBox, toyboxVar, 0, room:GetGridPosition(116), Vector(0,0), Isaac.GetPlayer(0));
-            end
-            s = toyboxEntity:GetSprite()
-			s:Play("Idle",true)
-			destroyed = false
-        end
-    else
-        toyboxEntity = nil
-    end
-end
-
-----------------------------------------------------------------------
-function bonusItems:updateToyboxState(entity)
-    local dist = 0
-
-	if toyboxEntity == nil then
-		return
-	end
-
-    for i = 1, Game():GetNumPlayers() do
-		player = Isaac.GetPlayer(i-1)
-		dist = toyboxEntity.Position:Distance(player.Position)
-        if dist < 25 and destroyed == false then
-            s = toyboxEntity:GetSprite()
-            s:Play("Use", true)
-        elseif dist >= 25 and destroyed == false then
-            s:Play("Idle", true)
-        elseif destroyed == true and (itemsDropped == true or itemsDropped == false) then
-            s:Play("Destroyed", true)
-        end
-    end
-end
+-- Pickup generation handlers
 ----------------------------------------------------------------------
 function choosePickupPool()
     local pickupPool = {
@@ -136,16 +87,71 @@ function generatePickups()
     if destroyed == false then
         for i = 1, 3 do
             pickupChoice = choosePickupPool()
-            Isaac.Spawn(5, pickupChoice, 0, toyboxEntity.Position, Vector(math.random(-10,10),math.random(-10,10)), nil)
+            Isaac.Spawn(5, pickupChoice, 0, toyboxEntity.Position, Vector(math.random(-10,10), math.random(-10,10)), nil)
+            -- silly method of making items spew out from the toybox, but it works and I like it this way
         end
     end
 end
 ----------------------------------------------------------------------
-function boxDamage(p1, p2, p3, flags, p4)
+-- Toybox generation and toybox state handlers
+----------------------------------------------------------------------
+function bonusItems:initToybox()
+    local room = Game():GetRoom()
+
+	if (Game():IsGreedMode() == false and Game():GetLevel():GetCurrentRoomIndex() == Game():GetLevel():GetStartingRoomIndex())
+    or (Game():IsGreedMode() == false and Game():GetLevel():GetCurrentRoomIndex() == 97 and Game():GetLevel():GetStage() == 9)
+	or (Game():IsGreedMode() == true  and Game():GetLevel():GetCurrentRoomIndex() == 98) then
+        local ent = Isaac.FindByType(toyBox, toyboxVar)
+        if #ent > 0 then
+			toyboxEntity = ent[1]
+			if destroyed == true then
+				sprite = toyboxEntity:GetSprite()
+				sprite:Play("Destroyed",true)
+			end
+			return
+		end
+
+        if (toyboxEntity == nil) or (toyboxEntity:Exists() == false) then
+			if Game():IsGreedMode() == true then
+			    toyboxEntity = Isaac.Spawn(toyBox, toyboxVar, 0, room:GetGridPosition(112), Vector(0,0), Isaac.GetPlayer(0));
+            else
+                toyboxEntity = Isaac.Spawn(toyBox, toyboxVar, 0, room:GetGridPosition(116), Vector(0,0), Isaac.GetPlayer(0));
+            end
+            sprite = toyboxEntity:GetSprite()
+			sprite:Play("Idle",true)
+			destroyed = false
+        end
+    else
+        toyboxEntity = nil
+    end
+end
+----------------------------------------------------------------------
+function bonusItems:updateToyboxState(entity)
+    local dist = 0
+
+	if toyboxEntity == nil then
+		return
+	end
+
+    for i = 1, Game():GetNumPlayers() do
+		player = Isaac.GetPlayer(i-1)
+		dist = toyboxEntity.Position:Distance(player.Position)
+        if dist < 25 and destroyed == false then
+            sprite = toyboxEntity:GetSprite()
+            sprite:Play("Use", true)
+        elseif dist >= 25 and destroyed == false then
+            sprite:Play("Idle", true)
+        elseif destroyed == true and (itemsDropped == true or itemsDropped == false) then
+            sprite:Play("Destroyed", true)
+        end
+    end
+end
+----------------------------------------------------------------------
+function boxDamage(p1, p2, p3, flags, p4) -- check done to generate pickups the first time the toybox is bombed
 	if toyboxEntity ~= nil then
 		if (flags & DamageFlag.DAMAGE_EXPLOSION) ~= 0 and destroyed == false then
-			s = toyboxEntity:GetSprite()
-			s:Play("Destroyed", true)   
+			sprite = toyboxEntity:GetSprite()
+			sprite:Play("Destroyed", true)   
             Isaac.Spawn(1000, 15, 0, toyboxEntity.Position, Vector(0,0), player)
             generatePickups()
             destroyed = true
@@ -154,32 +160,34 @@ function boxDamage(p1, p2, p3, flags, p4)
 	end
 end
 ----------------------------------------------------------------------
-function itemSpawnCheck()
+function itemSpawnCheck() -- check done to generate items the first time the chest is opened
     if toyboxEntity ~= nil then
         player = Isaac.GetPlayer(0)
         dist = toyboxEntity.Position:Distance(player.Position)
         if dist < 25 and itemsDropped == false then
-            bonusItems:itemsPlease(player)
+            itemsPlease(player)
             itemsDropped = true
         end
     end
 end
 ----------------------------------------------------------------------
+-- Cleanup/Support
+----------------------------------------------------------------------
 function resetTrackers()
-    itemsDropped = false
+    itemsDropped = false -- used exclusively to set a cap on the amount of times a toybox will generate items
 end
 ----------------------------------------------------------------------
 
 bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, bonusItems.initToybox)
 bonusItems:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, boxDamage)
 bonusItems:AddCallback(ModCallbacks.MC_NPC_UPDATE, bonusItems.updateToyboxState, toyBox)
+bonusItems:AddCallback(ModCallbacks.MC_PRE_NPC_COLLISION, itemSpawnCheck)  
 bonusItems:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, resetTrackers)
-bonusItems:AddCallback(ModCallbacks.MC_PRE_NPC_COLLISION, itemSpawnCheck)   
 
 --[[
-    I orginally tried to program a real solution to give Jacob and Esau compatibility, but there is so much
-    bullshit I don't understand when it came to callback logic I used to give/spawn items, I decided to opt for a workaround instead
+    I orginally tried to program a solution to give Jacob and Esau proper compatibility, but there is so much
+    bulls**t I don't understand when it came to callback logic I used to give/spawn items, I decided to opt for a workaround instead
 
-    pro tip: don't generate items that generate pickups before the level loads or else the game will crash
-    note: lil delirium and red key may also have game crashing properties, but I am completely oblivious as to why
+    pro tip:    don't generate items that generate pickups before the level loads or else the game will crash
+    note:       lil delirium and red key may also have game crashing properties, but I am completely oblivious as to why
 ]]
